58fadbaea410750f4d40dbe0278f2241084ff1c7
Daniel Isaac <63597272+DannStormy@users.noreply.github.com>
2020-08-31 19:52:13 +0100

    Update application.py

--- bucketlist-app/application.py
+++ bucketlist-app/application.py
@@ -27,10 +27,6 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-
-
 
 Session(app)
 filePath = os.path.abspath("database.db")
